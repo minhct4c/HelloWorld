f1486fc71047a344dbb199773d0f03750a5964cb
userKuro <minhtruogn@gmail.com>
2024-05-22 09:37:56 +0700

    initial commit!

---
+++ app/src/main/java/com/demo/helloworld/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.demo.helloworld
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textView : TextView = findViewById(R.id.textView)
+        textView.setText("Hello Minh, kotlin!")
+
+    }
+}
